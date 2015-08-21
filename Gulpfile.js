bfca08d88f92c19a065b240480c641c0be4a28c7
David Kinsella <david.kinsella@autotrader.co.uk>
2015-08-21 14:33:15 +0100

    browserify and bundles support

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,18 @@
 'use strict';
  
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var del = require('del');
 var livereload = require('gulp-livereload');
 var rename = require("gulp-rename2");
+var debug = require('gulp-debug');
+var glob = require('glob');
 /* stylesheet processing */
 var sass = require('gulp-sass');
+var globbing = require('gulp-css-globbing');
 var postcss = require('gulp-postcss');
 var assets  = require('postcss-assets');
 var sourcemaps = require('gulp-sourcemaps');
-var csscomb = require('gulp-csscomb');
 var autoprefixer = require('autoprefixer-core');
 var csslint = require('gulp-csslint');
 var minifycss = require('gulp-minify-css');
@@ -28,11 +31,11 @@ gulp.task('clean:css', function (cb) {
   });
 });
 gulp.task('clean:js', function (cb) {
-  del(['run/**/*.js'], function(err){
+  del(['run/**/*.js','run/**/*.js.map'], function(err){
     cb(err);
   });
 });
-gulp.task('clean:img', function (cb) {
+gulp.task('clean:assets', function (cb) {
   del(['run/assets'], function(err){
     cb(err);
   });
@@ -49,39 +52,43 @@ gulp.task('css:lint',function(){
 
 /* build the development CSS, readable format with sourcemap for debugging */
 gulp.task('css:dev', ['clean:css'], function(){
-  gulp.src('./source/**/*.scss')
+  gulp.src('./source/5-bundles/**/*.scss')
     .pipe(sourcemaps.init())
+    .pipe(globbing({
+        extensions: ['.scss']
+    }))
+    //.pipe(debug({title:'cssdev pre-sass',minimal:false}))
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([autoprefixer()]))
-    .pipe(csscomb())
+    .pipe(postcss([assets,autoprefixer]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./run/css'))
     .pipe(csslint())
     .pipe(csslint.reporter('text'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./run'))
     .pipe(livereload());
 })
  
 /* Build the production CSS, optimised as much as possible*/
 gulp.task('css:build', ['clean:css'], function(){
-  gulp.src('./source/**/*.scss')
+  gulp.src('./source/5-bundles/**/*.scss')
+    .pipe(globbing({
+        extensions: ['.scss']
+    }))
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([assets({
-      loadPaths: ['./source/**/*']
-    }), autoprefixer]))
+    .pipe(postcss([assets, autoprefixer]))
     .pipe(minifycss({processImport:false}))
+    .pipe(gulp.dest('./run/css'))
     .pipe(csslint())
     .pipe(csslint.reporter('text'))
-    .pipe(gulp.dest('./run'))
     .pipe(livereload());
 });
 
 /* watch the scss files and run the dev task */
-gulp.task('css:watch', function () {
+gulp.task('css:watch', ['css:dev'], function () {
   gulp.watch('./source/**/*.scss', ['css:dev']);
 });
 
 /* watch the scss files and run the build task */
-gulp.task('css:watch:build', function () {
+gulp.task('css:watch:build', ['css:build'], function () {
   gulp.watch('./source/**/*.scss', ['css:build']);
 });
 
@@ -94,47 +101,61 @@ gulp.task('js:lint',function(){
 });
 
 gulp.task('js:dev',['clean:js','js:lint'],function(){
-  var b = browserify({
-    entries: './source/main.js',
-    debug: true
-  });
-  return b.bundle()
-    .pipe(source('main.js'))
-    .pipe(buffer())
-    .pipe(jshint())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./run'))
-    .pipe(livereload());
+  var files = glob.sync('./source/5-bundles/**/*.js');
+  var bundleThis = function(srcArray) {
+    srcArray.forEach(function(sourceFile) {
+        var bundle = browserify([sourceFile],{debug:true}).bundle();
+        bundle
+        .pipe(source(sourceFile))
+        .pipe(buffer())
+        .pipe(rename(function (pathObj, filePath) {
+          return pathObj.join(
+            pathObj.dirname(filePath).replace(/source\/5-bundles\/?/,''),
+            pathObj.basename(filePath)
+          );
+        }))
+        .pipe(gulp.dest('./run/js'))
+        .pipe(livereload());
+    });
+  };
+  bundleThis(files);
 });
 
 gulp.task('js:build',['clean:js','js:lint'],function(){
-  var b = browserify({
-    entries: './source/main.js',
-    debug: true
-  });
-  return b.bundle()
-    .pipe(source('main.js'))
-    .pipe(buffer())
-    .pipe(jshint())
-    .pipe(uglify())
-    .pipe(gulp.dest('./run'))
-    .pipe(livereload());
+  var files = glob.sync('./source/5-bundles/**/*.js');
+  var bundleThis = function(srcArray) {
+    srcArray.forEach(function(sourceFile) {
+        var bundle = browserify([sourceFile],{debug:true}).bundle();
+        bundle
+        .pipe(source(sourceFile))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(rename(function (pathObj, filePath) {
+          return pathObj.join(
+            pathObj.dirname(filePath).replace(/source\/5-bundles\/?/,''),
+            pathObj.basename(filePath)
+          );
+        }))
+        .pipe(gulp.dest('./run/js'))
+        .pipe(livereload());
+    });
+  };
+  bundleThis(files);
 });
 
-gulp.task('js:watch',function(){
+gulp.task('js:watch', ['js:dev'], function(){
   gulp.watch('./source/**/*.js',['js:dev']);
 });
 
-/* image tasks */
-gulp.task('images', ['clean:img'], function(){
+/* assets tasks */
+gulp.task('assets', ['clean:assets'], function(){
   gulp.src(['./source/**/img/**/*.png','./source/**/img/**/*.svg','./source/**/img/**/*.jpg'])
-  .pipe(rename(function (pathObj, filePath) {
-    return pathObj.join(
-      pathObj.dirname(filePath).replace(/img\/?/,''),
-      pathObj.basename(filePath)
-    );
-  }, { verbose: true }))
+  // .pipe(rename(function (pathObj, filePath) {
+  //   return pathObj.join(
+  //     pathObj.dirname(filePath).replace(/img\/?/,''),
+  //     pathObj.basename(filePath)
+  //   );
+  // }, { verbose: true }))
   .pipe(gulp.dest('./run/assets'));
 });
 
@@ -142,6 +163,6 @@ gulp.task('images', ['clean:img'], function(){
 gulp.task('watch',['css:watch','js:watch'], function(){
   livereload.listen();
 });
-gulp.task('dev',['images','css:dev','js:dev']);
-gulp.task('build',['images','css:build','js:build']);
-gulp.task('default',['dev','watch']);
+gulp.task('dev',['assets','css:dev','js:dev']);
+gulp.task('build',['assets','css:build','js:build']);
+gulp.task('default',['watch']);
